37387a8ff4f5468c7755bc36a702999161c9ecb2
Varinder Kaur <kaurvarinder0209@gmail.com>
2023-04-16 23:15:50 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Quiz from './components/Quiz';
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App"> 
+    <>
+        <Quiz/>
+    </>
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p> */}
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+// import React from 'react'
+
+// function App() {
+//   return {
+//     <div> App </div>
+//   }
+// }
+
+// export default App
---
+++ src/Data/QuizData.js
@@ -0,0 +1,22 @@
+export const QuizData = [
+    {
+        question : "LPU is in which state?" ,
+        options : ["Punjab", "Mumbai", "Gujrat", "Assam"] ,
+        answer : 1
+    },
+    {
+        question : "Thakur College is in which state?" ,
+        options : ["Punjab", "Mumbai", "Gujrat", "Assam"] ,
+        answer : 2
+    },
+    {
+        question : "Vit Vellore is in which state?" ,
+        options : ["Punjab", "Mumbai", "Chennai", "Assam"] ,
+        answer : 3
+    },
+    {
+        question : "Amity is in which state?" ,
+        options : ["Punjab", "Mumbai", "Gujrat", "Delhi"] ,
+        answer : 4
+    },
+];
